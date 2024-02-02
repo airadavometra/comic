0b72d359e414a6e086b23b4f28fdf3326a3b3841
Daria Artemova <airadavometra@gmail.com>
2024-02-02 23:42:57 +0100

    change 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,17 +1,15 @@
 import { LinkWithUnderline } from "@components/LinkWithUnderline/LinkWithUnderline";
 import s from "./NotFoundPage.module.css";
-import { Title } from "@components/Title/Title";
 import { Arrow } from "@icons/Arrow";
 
 export const NotFoundPage = () => {
   return (
     <main className={s.main}>
-      <Title>Sorry, page you are looking for does not exist...</Title>
+      <h1 className={s.title}>404</h1>
+      <span>Sorry, page you are looking for does not exist...</span>
       <div className={s.linkContainer}>
         <Arrow className={s.icon} />
-        <LinkWithUnderline href={"/"} className={s.link}>
-          Go back to main page
-        </LinkWithUnderline>
+        <LinkWithUnderline href={"/"}>Go back to main page</LinkWithUnderline>
       </div>
     </main>
   );
